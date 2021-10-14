c8c9da891addcc8329ac5175b54f4732da89500b
vietthangc1 <65752240+vietthangc1@users.noreply.github.com>
2021-10-15 00:55:45 +0700

    initial commit

---
+++ checkapp/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from datetime import datetime
+import uuid
+
+# Create your models here.
+class Customers(models.Model):
+  user_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+  name = models.CharField(max_length=256)
+  CardId = models.CharField(max_length=20, blank=True)
+  YearofBirth = models.CharField(blank=True, max_length=4)
+  email = models.EmailField(default="@gmail.com")
+  phone = models.CharField(max_length=15, blank=True)
+  school = models.CharField(max_length=256)
+  created_time = models.DateTimeField(default=datetime.now, editable=False)
+  times_of_scan = models.IntegerField(default=0, editable=False)
+  last_scanned = models.DateTimeField(default=datetime.now, editable=False)
+
+  @property
+  def qr_link_local(self):
+      "Returns the link of qr code for user_id."
+      return "http://127.0.0.1:8000/app/scan/"+str(self.user_id)
+      
+  @property
+  def qr_link_heroku(self):
+      "Returns the link of qr code for user_id."
+      return "https://ftuzone-checkqr-pvthang.herokuapp.com/app/scan/"+str(self.user_id)
+
+  def __str__(self):
+    return self.name+self.YearofBirth+self.school
